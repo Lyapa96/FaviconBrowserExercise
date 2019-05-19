9e8e8a489ddf3f7cf82756c80f0ccf37b2583c7d
lms <lms@skbkontur.ru>
2019-05-19 10:51:47 +0500

    init

---
+++ FaviconBrowser/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace FaviconBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly List<string> SDomains = new List<string>
+                                                             {
+                                                                 "google.com",
+                                                                 "bing.com",
+                                                                 "oreilly.com",
+                                                                 "microsoft.com",
+                                                                 "facebook.com",
+                                                                 "twitter.com",
+                                                                 "reddit.com",
+                                                                 "vk.com",
+                                                                 "bbc.co.uk"
+                                                             };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            foreach (var domain in SDomains)
+            {
+                AddAFavicon(domain);
+            }
+        }
+
+        private void AddAFavicon(string domain)
+        {
+            var webClient = new WebClient();
+            var bytes = webClient.DownloadData("http://" + domain + "/favicon.ico");
+            var imageControl = MakeImageControl(bytes);
+            m_WrapPanel.Children.Add(imageControl);
+        }
+
+        private static Image MakeImageControl(byte[] bytes)
+        {
+            var imageControl = new Image();
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(bytes);
+            bitmapImage.EndInit();
+            imageControl.Source = bitmapImage;
+            imageControl.Width = 16;
+            imageControl.Height = 16;
+            return imageControl;
+        }
+    }
+}
